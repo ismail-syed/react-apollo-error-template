b6d1101061e561c4c06e07f5bc9c0cd8c726e615
Ismail Syed <isyed867@gmail.com>
2019-09-19 10:11:08 -0400

    Show breakage

--- src/index.js
+++ src/index.js
@@ -3,16 +3,37 @@ import { render } from "react-dom";
 import { ApolloClient } from "apollo-client";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloLink } from "apollo-link";
 
 import { link } from "./graphql/link";
 import App from "./App";
 
 import "./index.css";
 
+const timeStartLink = new ApolloLink((operation, forward) => {
+  console.log("timeStartLink running");
+  operation.setContext({ start: new Date() });
+  return forward(operation);
+});
+
+const logTimeLink = new ApolloLink((operation, forward) => {
+  return forward(operation).map(data => {
+    const time = new Date() - operation.getContext().start;
+    console.log(
+      `logTimeLink running: operation ${operation.operationName} took ${time} to complete`
+    );
+    return data;
+  });
+});
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link
+  link: link
+  // link: timeStartLink.concat(logTimeLink).concat(link)
 });
+
+// this should proporgate the link changes?
+client.link = timeStartLink.concat(logTimeLink).concat(link);
 
 render(
   <ApolloProvider client={client}>
